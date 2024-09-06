339e5053ae43b187ad9f1c2024434dfc2dc44d3b
UlvFoerlev <ulvfoerlev@gmail.com>
2024-09-06 19:49:49 +0200

    Light Level Slider

--- actions/set_light/set_light_action.py
+++ actions/set_light/set_light_action.py
@@ -18,6 +18,35 @@ class SetLightAction(LightAction):
 
         return self.plugin_base.backend.lights_cache.get(self.selected_light)
 
+    def get_config_rows(self):
+        self.refresh_hub()
+
+        base = super().get_config_rows()
+        self.setup_light_level_settings()
+
+        return base
+
+    def setup_light_level_settings(self, base):
+        self.light_level_scale = ScaleRow(
+            title=self.plugin_base.lm.get("action.generic.volume"),
+            value=self.light_level,
+            min=1,
+            max=100,
+            step=1,
+            text_left="1",
+            text_right="100",
+        )
+        self.light_level_scale.scale.set_draw_value(True)
+
+        self.light_level_scale.adjustment.connect(
+            "value-changed", self.on_light_level_scale_change
+        )
+
+        base.append(self.light_level_scale)
+
+    def on_light_level_scale_change(self, entry):
+        self.light_level = entry.get_value()
+
     def on_key_down(self):
         # Initiate hub if not initiated yet
         if not self.plugin_base.backend.hub:
