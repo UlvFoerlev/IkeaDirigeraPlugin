2ee583f8974c3879f4f5f185262bc9c6722b6717
UlvFoerlev <ulvfoerlev@gmail.com>
2024-09-06 19:08:26 +0200

    Moved some logic to page load

--- actions/ikea_action_base.py
+++ actions/ikea_action_base.py
@@ -7,9 +7,6 @@ class IkeaActionBase(ActionBase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        if self.hub_ip and self.hub_token:
-            self.setup_hub(ip=self.hub_ip, token=self.hub_token)
-
     def _get_property(
         self, key: str, default: Any = None, enforce_type: type | None = None
     ) -> Any:
@@ -44,6 +41,9 @@ class IkeaActionBase(ActionBase):
         base.append(self.token_input)
 
     def get_config_rows(self):
+        if self.hub_ip and self.hub_token:
+            self.setup_hub(ip=self.hub_ip, token=self.hub_token)
+
         base = super().get_config_rows()
         self.setup_hub_info_input(base=base)
 
