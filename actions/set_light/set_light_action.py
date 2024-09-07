a0c587241b3c8a3053a9f4531a2523c552edd800
UlvFoerlev <ulvfoerlev@gmail.com>
2024-09-07 13:04:35 +0200

    Disabling temperature control, since it clashes with my personal needs, and I'm on a deadline. Will return to it later

--- actions/set_light/set_light_action.py
+++ actions/set_light/set_light_action.py
@@ -21,7 +21,7 @@ class SetLightAction(LightAction):
     def get_config_rows(self):
         base = super().get_config_rows()
         self.setup_light_level_settings(base=base)
-        self.setup_color_temperature_settings(base=base)
+        # self.setup_color_temperature_settings(base=base)
         self.setup_color_hue_settings(base=base)
         self.setup_color_saturation_settings(base=base)
 
@@ -45,32 +45,32 @@ class SetLightAction(LightAction):
 
         base.append(self.light_level_scale)
 
-    def setup_color_temperature_settings(self, base):
-        temp_min = self.light.attributes.color_temperature_min if self.light else 0
-        temp_max = self.light.attributes.color_temperature_max if self.light else 100
+    # def setup_color_temperature_settings(self, base):
+    #     temp_min = self.light.attributes.color_temperature_min if self.light else 0
+    #     temp_max = self.light.attributes.color_temperature_max if self.light else 100
 
-        if temp_min > temp_max:
-            temp_min, temp_max = temp_max, temp_min
+    #     if temp_min > temp_max:
+    #         temp_min, temp_max = temp_max, temp_min
 
-        # Clamp values
-        self.color_temperature = max(min(self.color_temperature, temp_max), temp_min)
+    #     # Clamp values
+    #     self.color_temperature = max(min(self.color_temperature, temp_max), temp_min)
 
-        self.color_temperature_scale = ScaleRow(
-            title=self.plugin_base.lm.get("action.set-light.temperature"),
-            value=self.color_temperature,
-            min=temp_min,
-            max=temp_max,
-            step=1,
-            text_left=str(temp_min),
-            text_right=str(temp_max),
-        )
-        self.color_temperature_scale.scale.set_draw_value(True)
+    #     self.color_temperature_scale = ScaleRow(
+    #         title=self.plugin_base.lm.get("action.set-light.temperature"),
+    #         value=self.color_temperature,
+    #         min=temp_min,
+    #         max=temp_max,
+    #         step=1,
+    #         text_left=str(temp_min),
+    #         text_right=str(temp_max),
+    #     )
+    #     self.color_temperature_scale.scale.set_draw_value(True)
 
-        self.color_temperature_scale.adjustment.connect(
-            "value-changed", self.on_color_temperature_scale_change
-        )
+    #     self.color_temperature_scale.adjustment.connect(
+    #         "value-changed", self.on_color_temperature_scale_change
+    #     )
 
-        base.append(self.color_temperature_scale)
+    #     base.append(self.color_temperature_scale)
 
     def setup_color_hue_settings(self, base):
         self.color_hue_scale = ScaleRow(
