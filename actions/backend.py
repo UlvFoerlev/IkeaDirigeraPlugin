dd535a5dcbeb65afa9742223d3350546c1ffdb15
UlvFoerlev <ulvfoerlev@gmail.com>
2024-09-07 14:42:18 +0200

    Falsy zero an enternal lesson in checking values the correct way

--- actions/backend.py
+++ actions/backend.py
@@ -71,9 +71,9 @@ class Backend(BackendBase):
         #     )
 
         if hue or saturation:
-            new_hue = hue or light.attributes.color_hue
+            new_hue = hue if hue is not None else light.attributes.color_hue
             new_hue = clamp(new_hue, 0, 360)
-            new_saturation = saturation or light.attributes.color_saturation
+            new_saturation = saturation if saturation is not None or light.attributes.color_saturation
             new_saturation = clamp(new_saturation, 0.0, 1.0)
 
             light.set_light_color(hue=new_hue, saturation=new_saturation)
