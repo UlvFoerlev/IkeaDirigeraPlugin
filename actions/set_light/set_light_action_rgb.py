9f1387ad3ffd4eaae89810827186dfe7778c206e
UlvFoerlev <ulvfoerlev@gmail.com>
2024-09-07 14:22:46 +0200

    More type fixes

--- actions/set_light/set_light_action_rgb.py
+++ actions/set_light/set_light_action_rgb.py
@@ -135,7 +135,8 @@ class SetLightRGBAction(SetLightAction):
         return delta / dividend
 
     def _calculate_light_level_from_rgb(self, r: float, g: float, b: float) -> int:
-        return int(float(max([r, g, b]) + min([r, g, b])) / 2.0) * 100
+        print("l calc: ", (float(max([r, g, b]) + min([r, g, b])) / 2.0))
+        return int((float(max([r, g, b]) + min([r, g, b])) / 2.0) * 100)
 
     def on_color_red_slider_change(self, entry):
         self.color_red = int(entry.get_value())
